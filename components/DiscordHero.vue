8080c933363afc3b398de262a35e1cb4d03368e0
sneakycrow <zach@sneakycrow.dev>
2024-06-22 14:17:20 -0700

    fix DiscordHero structure

--- components/DiscordHero.vue
+++ components/DiscordHero.vue
@@ -1,19 +1,28 @@
 <template>
-    <div class="bg-join-community bg-cover bg-center my-24">
-        <a class="w-full h-full flex px-4 pt-12 pb-36 sm:pb-12" href="https://discord.gg/your-discord-invite-link" target="_blank" rel="noopener noreferrer">
-            <div class="mx-4 sm:mx-14">
-                <h1 class="text-md sm:text-2xl pb-2 uppercase font-bold mb-0">Join the community</h1>
-                <img class="w-7/8 sm:w-1/3 pb-2" src="/the_balance.svg">
-                <div class="flex flex-row items-center">
-                    <h2 class="text-md sm:text-2xl font-bold mb-0">discord.gg/thebalanceffxiv</h2>
-                </div>
-            </div>
-        </a>
+  <a class="block" href="https://discord.gg/thebalanceffxiv">
+    <div
+      class="bg-join-community bg-cover bg-center my-24 px-4 pt-12 pb-36 sm:pb-12"
+    >
+      <div class="container mx-auto">
+        <div class="mx-4 sm:mx-14">
+          <h1 class="text-md sm:text-2xl pb-2 uppercase font-bold mb-0">
+            Join the community
+          </h1>
+          <img class="w-7/8 sm:w-1/3 pb-2" src="/the_balance.svg" />
+          <div class="flex flex-row items-center">
+            <img class="w-auto mr-4" src="/icons/discord_logo_white.svg" />
+            <h2 class="text-md sm:text-2xl font-bold mb-0">
+              discord.gg/thebalanceffxiv
+            </h2>
+          </div>
+        </div>
+      </div>
     </div>
+  </a>
 </template>
 
 <script>
 export default {
-    name: 'DiscordHero',
-}
+  name: "DiscordHero",
+};
 </script>
