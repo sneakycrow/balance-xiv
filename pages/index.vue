89bddc96c8d0b617cf608b26ceef6400853747ca
sneakycrow <zach@sneakycrow.dev>
2024-06-22 14:36:31 -0700

    add footer component

--- pages/index.vue
+++ pages/index.vue
@@ -4,5 +4,6 @@
     <!-- Position RoleSlider slightly over the header -->
     <RoleSlider class="max-w-screen-xl -mt-10 mx-auto" />
     <DiscordHero />
+    <BalanceFooter />
   </main>
 </template>
