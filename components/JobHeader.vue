2ed9568796cfe9b55f6c9cbc227fdeeeafb989a9
dpcodes <d.potvin98@gmail.com>
2024-06-22 15:28:17 -0400

    job header image and job name

--- components/JobHeader.vue
+++ components/JobHeader.vue
@@ -1,17 +1,24 @@
 <template>
-  <header
-    :class="`bg-${job} bg-top bg-cover relative h-64 md:h-96 mt-10 lg:mt-0`"
-  />
   <div
-    class="absolute top-16 left-0 w-full flex items-center justify-center my-20 z-10"
-  >
-    <a href="/" class="flex items-center px-10 sm:px-0 py-4">
-      <img src="/the_balance.svg" alt="The Balance" class="w-96 lg:w-full" />
-    </a>
+    :class="`bg-${job} w-full -z-1 bg-top bg-cover absolute top-0 h-64 md:h-96`"
+  />
+  <div class="w-full h-64 md:h-96 z-10 pl-32 sm:pl-48 lg:pl-60 pt-16 md:pt-24">
+    <div class="flex items-center">
+      <img
+        class="w-1/12 xl:w-32 md:h-20 xl:h-32"
+        src="/assets/jobs/tanks/paladin/icon.svg"
+        alt="Paladin Job Icon"
+      />
+      <h1
+        class="capitalize text-3xl md:text-4xl xl:text-6xl font-semibold filter ml-4 drop-shadow-lg-tanks mb-0"
+      >
+        {{ job }}
+      </h1>
+    </div>
   </div>
 </template>
 
 <script setup>
-const route = useRoute();
-const job = route.params.job;
+const { params } = useRoute();
+const job = params.job;
 </script>
