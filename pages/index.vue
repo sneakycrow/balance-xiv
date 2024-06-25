ddff0b0c3829e7e0786def8653b973ba6f98ba49
dpcodes <d.potvin98@gmail.com>
2024-06-25 19:38:22 -0400

    pr comment

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <!-- Position RoleSlider slightly over the header -->
+  <!-- Position RoleSlider slightly over the header, which is expected to be rendered directly above this -->
   <RoleSlider class="responsive-container -mt-10" />
   <DiscordHero />
 </template>
