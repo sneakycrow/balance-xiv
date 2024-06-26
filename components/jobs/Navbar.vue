31974a6f8da16313904759b305c1f95751d0e664
David Potvin <d.potvin98@gmail.com>
2024-06-26 14:39:07 -0400

    Update components/jobs/Navbar.vue
    Co-authored-by: Zachary Corvidae <zach@sneakycrow.dev>

--- components/jobs/Navbar.vue
+++ components/jobs/Navbar.vue
@@ -5,6 +5,7 @@
         <a href="/" class="flex items-center gap-2 py-3 md:py-4">
           <NuxtImg class="h-5 lg:h-8" src="/the_balance_icon.svg" />
           <NuxtImg class="h-3 lg:h-5" src="/the_balance.svg" />
+          <!-- TODO: Get the two images above as a single image -->
         </a>
 
         <div class="flex items-center gap-8">
