4156ef63dc1f0b4414d3efdbac0672dc4200a79e
dpcodes <d.potvin98@gmail.com>
2024-06-26 08:28:08 -0400

    typo

--- pages/credits.vue
+++ pages/credits.vue
@@ -1,3 +1,3 @@
 <template>
-  <ContentDoc class="p-10 responsive-container md: md:my-8 text-lg rounded bg-card-light" />
+  <ContentDoc class="p-10 responsive-container md:my-8 text-lg rounded bg-card-light" />
 </template>
